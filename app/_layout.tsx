212b3f7757326105e474615373f6690853d94d96
David Carboni <david@carboni.io>
2025-06-27 23:28:42 +0100

    Fix up routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Timer: { duration: number; };
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="timer" />
+    </Stack>
+  );
 }
